4673f78d723b32fd0da2a557f8935b9ce4835d3d
rokmaker <rokmaker@naver.com>
2024-02-26 20:55:14 +0900

    Update ffmpeg_h264_mov.sh

--- ffmpeg_script/ffmpeg_h264_mov.sh
+++ ffmpeg_script/ffmpeg_h264_mov.sh
@@ -5,11 +5,21 @@
 # Find directories and read them into an array
 readarray -t comp_paths < <(find "./png" -mindepth 1 -maxdepth 1 -type d)
 
+# Create folders if they do not exist
+mkdir -p "./mov" "./png/_out"
+
 # Loop through each path in the array
 for comp_path in "${comp_paths[@]}"; do
     echo " "
     echo "Processing $comp_path"
     echo " "
+    # If _out folder in array, skip it
+    if [[ $comp_path == *"_out"* ]]; then
+        echo "Skipping $comp_path"
+        echo " "
+        continue
+    fi
+    # Set the variables
     default_name=$(basename "$comp_path")
     input_png_file="$comp_path/"$default_name"_%04d.png"
     output_mov_folder="./mov"
@@ -17,7 +27,14 @@ for comp_path in "${comp_paths[@]}"; do
     # ffmpeg command here
     ffmpeg -f image2 -r 24 -start_number 0001 -i "$input_png_file" -c:v libx264 -b:v 20000k -colorspace 1 -color_primaries 1 -color_trc 1 -color_range tv -pix_fmt yuv420p -y "$output_mov_file"
     echo " "
+    # Move the processed comp_path folder to the _out folder
+    mv "$comp_path" "./png/_out"
     echo "Completed processing $comp_path"
     echo " "
-    #sleep 1 # Prevents the script from crashing when processing multiple files
 done
+
+# Notify user that the all of the rendering processes are complete
+echo -e "\033[0;31mRendering complete\033[0m"
+echo " "
+
+# End of script
